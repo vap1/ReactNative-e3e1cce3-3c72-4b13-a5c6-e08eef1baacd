789d029960654827475434131c3ef944b2bea1a6
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 17:36:53 +0530

    First Commit

--- src/apis/DataDisplayApi.ts
+++ src/apis/DataDisplayApi.ts
@@ -4,15 +4,11 @@ import { DataDisplayRequest, DataDisplayResponse } from '../types/UserTypes';
 
 const BASE_URL = 'http://your-api-base-url.com'; // Replace with your actual API base URL
 
-const DataDisplayApi = {
-  dataDisplay: async (request: DataDisplayRequest): Promise<DataDisplayResponse> => {
-    try {
-      const response = await axios.get(`${BASE_URL}/data-display`);
-      return response.data;
-    } catch (error) {
-      throw new Error('Failed to retrieve data for display');
-    }
-  },
+export const dataDisplayApi = async (request: DataDisplayRequest): Promise<DataDisplayResponse> => {
+  try {
+    const response = await axios.get(`${BASE_URL}/data-display`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to retrieve data for display');
+  }
 };
-
-export default DataDisplayApi;
